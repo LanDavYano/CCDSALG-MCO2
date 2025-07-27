54a7eccf4bba570f0ceae7dc7713c295915cf640
LanDavYano <lance_jardiniano@dlsu.edu.ph>
2025-07-27 13:46:38 +0000

    yes

--- graphOpps.h
+++ graphOpps.h
@@ -3,11 +3,13 @@
 #include "graph.h"
 #include "queue.h"
 #include "stack.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 void breadthFirst(const char* key, Graph *g);
-
-depthFirst();
-pathCheck()
-minSpanTree()
+void depthFirst(Graph *g, const char* start);
+void pathCheck(Graph *g, const char* startVertex, const char* targetVertex);
+void minSpanTree();
+int shortestPath(Graph *g, const char* source, const char* destination);
 
 #endif
