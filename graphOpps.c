0278553f65e6e87c8a1c7c3e011eb2522bcb554f
lennardlee <152157274+lennardlee@users.noreply.github.com>
2025-07-27 10:41:16 +0000

    finished command 11 for MST

--- graphOpps.c
+++ graphOpps.c
@@ -194,36 +194,131 @@ void pathCheck(Graph *g, const char* startVertex, const char* targetVertex){
 }
 
 
+/* creating the min-span tree*/
+void createMSTKruskals(Graph *g, int[] parent, Edge edges, Edge edges_mst){
 
-function minSpanTree(Graph *g){
-    char adjacent[256][256];
-    char temp[256];
-    int adjacentCount;
-    int alreadyVisited;
-    int i, j;
+    // int parent[MAX_VERTICES]; // adjust size as needed
+    // Edge edges[MAX_VERTICES * MAX_VERTICES];
 
-    /* Reset adjacentCount for each vertex */
-    adjacentCount = 0;
-    
-    /* Find all adjacent vertices that haven't been visited */
-    for (i = 0; i < g->vertexCount; i++) {
-        if (checkEdge(g, cur, g->vertices[i].name)) {
-            /* Check if already visited */
-            alreadyVisited = 0;
-            for (j = 0; j < visitedCount; j++) {
-                if (strcmp(visited[j], g->vertices[i].name) == 0) {
-                    alreadyVisited = 1;
-                    break;
-                } 
-            }
-            
-            /* If not visited, add to adjacent list */
-            if (!alreadyVisited) {
-                strcpy(adjacent[adjacentCount++], g->vertices[i].name);
+    //for the edge count
+    int edgeCount = 0;
+    //for the temp variable
+    int edges_mst_count = 0;
+
+    for (int i = 0; i < g->vertexCount; i++) {
+        for (int j = i + 1; j < g->vertexCount; j++) {
+            if (g->adj[i][j]) {
+                strcpy(edges[edgeCount].source, g->vertices[i].name); // SOURCES
+                strcpy(edges[edgeCount].dest, g->vertices[j].name);   // DESTINATIONS
+                edges[edgeCount].weight = g->weight[i][j];            // WEIGHT
+                edgeCount++; 
             }
         }
     }
 
+    //sorts the edge by weight and alphabetically
+    edgeSorter(edges, edgeCount);
+
+    //create the set of parents first to check if it creates a cycle
+    createSet(g->vertexCount);
+
+    //now we build the MST
+    for(int o = 0; o<edgeCount;o++){
+
+        //getting the index passing a 
+        int s = getIndex(g, edges[o].source);
+        int d = getIndex(g, edges[o].dest);
+        //find muna kung ano result ng findRoot
+        int rootS = findRoot(s);
+        int rootD = findRoot(d);
+
+        if(rootS != rootD){
+            //adding the MST to the set
+            mergeSet(rootS,rootD);
+            //adding to the new edge array
+            strcpy(edges_mst[edges_mst_count].source, edges[o].source);
+            strcpy(edges_mst[edges_mst_count].dest, edges[o].dest);
+            strcpy(edges_mst[edges_mst_count].weight, edges[o].weight);
+            edges_mst_count++;
+        }
+    }
 }
 
 
+void printGraph(Graph *g, Edge edges_mst[]){
+
+    int l, i;
+
+    //prints graph header
+    printf("G = {V, E}\n");
+
+    //prints all the vertices
+    printf("V = {");
+    for(l=0;l<g->vertexCount;l++){
+        printf("%s, ",g[0]->vertices[l].name);
+    }
+    printf("}");
+
+    printf("E= {\n");
+        for(i=0;i<edges_mst.length;i++){
+            printf("(%s, %s, %d),\n",edges_mst[i].source,edges_mst[i].dest,edges_mst[i].weight )
+        }
+    printf("}\n");
+}
+
+/* sorting the edge by weight and lexicographically source first*/
+void edgeSorter(Edge edges[], int edgeCount) {
+    for (int i = 0; i < edgeCount - 1; i++) {
+        for (int j = 0; j < edgeCount - i - 1; j++) {
+            //sort by weight first
+            if (edges[j].weight > edges[j + 1].weight) {
+                Edge temp = edges[j];
+                edges[j] = edges[j + 1];
+                edges[j + 1] = temp;
+
+            } else if (edges[j].weight == edges[j + 1].weight) {
+
+                //if its the same weight check if the source is greater than the edge
+                if (strcmp(edges[j].source, edges[j + 1].source) > 0) {
+                    Edge temp = edges[j];
+                    edges[j] = edges[j + 1];
+                    edges[j + 1] = temp;
+                }
+            }
+
+        }
+    }
+}
+
+
+/* CREATING THE SET */
+void createSet(int vertexCount) {
+    for (int k = 0; k < vertexCount; k++) {
+        parent[k] = k;
+    }
+}
+
+// returning the root of the one you are adding to
+int findRoot(int x){
+
+    if(parent[x] != x){
+        return findRoot(x);
+    }
+
+    return x;
+}
+
+//merging the set/adding it to the MST
+void mergeSet(int p, int v){
+    parent[p] = v;
+}
+
+//gets the index of the struct
+int getIndex(Graph *g, char *name) {
+    for (int i = 0; i < g->vertexCount; i++) {
+        if (strcmp(g->vertices[i].name, name) == 0) {
+            return i;  // Found a match
+        }
+    }
+    return -1;  // Not found
+}
--- graphOpps.h
+++ graphOpps.h
@@ -8,6 +8,12 @@ void breadthFirst(const char* key, Graph *g);
 
 depthFirst();
 pathCheck()
-minSpanTree()
+void createMSTKruskals(Graph *g, int[] parent, Edge edges, Edge edges_mst);
+void printGraph(Graph *g, Edge edges_mst[]);
+void edgeSorter(Edge edges[], int edgeCount);
+void createSet(int vertexCount);
+int findRoot(int x);
+void mergeSet(int p, int v);
+int getIndex(Graph *g, char *name);
 
 #endif
